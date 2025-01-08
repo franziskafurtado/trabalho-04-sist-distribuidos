327df08cc44795b860c5ecdc1e4251ac47405339
PHSecchi <pedrohsecchi@gmail.com>
2025-01-08 14:36:08 -0300

    Correções na lib

--- backend/myRabbit/util.py
+++ backend/myRabbit/util.py
@@ -23,20 +23,23 @@ def publish_message(channel, exchange, routing_key, message):
     )
     print(f"Mensagem enviada para o tópico '{routing_key}': {message}")
 
-def process_events(host, queue_name, func_callback):
+def process_events(host, exchange, routing_key, func_callback):
     connection = pika.BlockingConnection(pika.ConnectionParameters(host=host))
     channel = connection.channel()
-    channel.queue_declare(queue=queue_name, durable=True)
+    #channel.queue_declare(queue=queue_name)
+    result = channel.queue_declare(queue='', exclusive=False)
+    queue_name = result.method.queue 
+    channel.queue_bind(exchange=exchange, queue=queue_name, routing_key=routing_key)
 
     def callback(ch, method, properties, body):
         event = json.loads(body)
-        print(f"Evento recebido no {queue_name}: {event}")
+        print(f"Evento recebido no {routing_key}: {event}")
         ch.basic_ack(delivery_tag=method.delivery_tag)
         return func_callback(event)
 
     channel.basic_consume(queue=queue_name, on_message_callback=callback)
     channel.start_consuming()  # Inicia o consumo
 
-def start_event_consumers(host,topics):
+def start_event_consumers(host,exchange, topics):
     for topic in topics:
-        Thread(target=process_events, args=(host, topic['queueName'], topic['func']), daemon=True).start()
+        Thread(target=process_events, args=(host, exchange, topic['queueName'], topic['func']), daemon=True).start()
